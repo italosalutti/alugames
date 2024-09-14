9350cc060d58958f5f2b112f87345405760ca108
Italo Salutti <salutti@gmail.com>
2024-09-14 12:04:50 -0300

    Finalização do código com alteração de status na imagem dos jogos e botões

--- js/app.js
+++ js/app.js
@@ -2,7 +2,15 @@ function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
-    let nomeDoJogo = gameClicado.querySelector('.dashboard__item__name');
 
-    alert(nomeDoJogo.textContent);
+    if(imagem.classList.contains('dashboard__item__img--rented')){
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
+
 }
